ee98475e5b34e88d9559a7e3f6323e28ec0fa368
ivan <i.s.ivanov2307@gmail.com>
2022-03-02 22:51:07 +0300

    add default headers to WebClient

--- src/main/java/com/iivanov/cleverdevtestnewsystem/config/WebClientConfig.java
+++ src/main/java/com/iivanov/cleverdevtestnewsystem/config/WebClientConfig.java
@@ -6,6 +6,8 @@ import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
@@ -35,6 +37,10 @@ public class WebClientConfig {
 
         return WebClient.builder()
             .baseUrl(urlOldSystem)
+            .defaultHeaders(headers -> {
+                headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+                headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+            })
             .clientConnector(new ReactorClientHttpConnector(httpClient))
             .build();
     }
