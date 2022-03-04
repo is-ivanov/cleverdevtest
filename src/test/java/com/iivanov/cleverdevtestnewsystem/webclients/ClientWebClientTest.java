3d3a2a8de07c626e241412124973ecb8bb2a41ab
ivan <i.s.ivanov2307@gmail.com>
2022-03-04 20:06:47 +0300

    add test in ClientWebClientTest

--- src/test/java/com/iivanov/cleverdevtestnewsystem/webclients/ClientWebClientTest.java
+++ src/test/java/com/iivanov/cleverdevtestnewsystem/webclients/ClientWebClientTest.java
@@ -22,54 +22,96 @@ class ClientWebClientTest {
     private ClientWebClient clientWebClient;
 
     @BeforeAll
-    static void setUp() throws IOException {
+    static void beforeAll() throws IOException {
         mockServer = new MockWebServer();
         mockServer.start();
     }
 
     @AfterAll
-    static void tearDown() throws IOException {
+    static void afterAll() throws IOException {
         mockServer.shutdown();
     }
 
     @BeforeEach
-    void initialize() {
+    void setUp() {
         String baseUrl = String.format("http://localhost:%s",
             mockServer.getPort());
         WebClient webClient = WebClient.create(baseUrl);
         this.clientWebClient = new ClientWebClient(webClient);
     }
 
-    @Test
-    @DisplayName("getClients")
-    void getClients() throws InterruptedException {
-        String jsonBody = "[{" +
-            "\"agency\": \"" + AGENCY_FIRST_PATIENT + "\"," +
-            "\"createdDateTime\": \"2021-10-20 03:10:24\"," +
-            "\"dob\": \"1960-02-02\"," +
-            "\"firstName\": \""  + NAME_FIRST_PATIENT + "\"," +
-            "\"guid\": \"" + GUID_FIRST_PATIENT + "\"," +
-            "\"lastName\": \"" + LAST_NAME_FIRST_PATIENT + "\"," +
-            "\"status\": \"ACTIVE\"" +
-            "}]";
-        mockServer.enqueue(
-            new MockResponse().setResponseCode(200)
-            .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .setBody(jsonBody)
-        );
+    @Nested
+    @DisplayName("test 'getClients' method")
+    class GetClientsTest {
 
-        List<ClientResponseDto> clients = clientWebClient.getClients();
-        RecordedRequest request = mockServer.takeRequest();
+        @Test
+        @DisplayName("when server send json with one object then should return " +
+            "List<ClientResponseDto> with size 1")
+        void getClients() throws InterruptedException {
+            String jsonBody = "[{" +
+                "\"agency\": \"" + AGENCY_FIRST_PATIENT + "\"," +
+                "\"createdDateTime\": \"2021-10-20 03:10:24\"," +
+                "\"dob\": \"1960-02-02\"," +
+                "\"firstName\": \"" + NAME_FIRST_PATIENT + "\"," +
+                "\"guid\": \"" + GUID_FIRST_PATIENT + "\"," +
+                "\"lastName\": \"" + LAST_NAME_FIRST_PATIENT + "\"," +
+                "\"status\": \"ACTIVE\"" +
+                "}]";
+            mockServer.enqueue(
+                new MockResponse().setResponseCode(200)
+                    .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .setBody(jsonBody)
+            );
 
-        assertThat(request.getMethod()).isEqualTo("POST");
-        assertThat(request.getPath()).contains("/clients");
+            List<ClientResponseDto> clients = clientWebClient.getClients();
+            RecordedRequest request = mockServer.takeRequest();
 
-        assertThat(clients).hasSize(1);
-        ClientResponseDto clientResponseDto = clients.get(0);
-        assertThat(clientResponseDto.getGuid()).isEqualTo(GUID_FIRST_PATIENT);
-        assertThat(clientResponseDto.getAgency()).isEqualTo(AGENCY_FIRST_PATIENT);
-        assertThat(clientResponseDto.getFirstName()).isEqualTo(NAME_FIRST_PATIENT);
-        assertThat(clientResponseDto.getLastName()).isEqualTo(LAST_NAME_FIRST_PATIENT);
+            assertThat(request.getMethod()).isEqualTo("POST");
+            assertThat(request.getPath()).isEqualTo("/clients");
+
+            assertThat(clients).hasSize(1);
+            ClientResponseDto clientResponseDto = clients.get(0);
+            assertThat(clientResponseDto.getGuid()).isEqualTo(GUID_FIRST_PATIENT);
+            assertThat(clientResponseDto.getAgency()).isEqualTo(AGENCY_FIRST_PATIENT);
+            assertThat(clientResponseDto.getFirstName()).isEqualTo(NAME_FIRST_PATIENT);
+            assertThat(clientResponseDto.getLastName()).isEqualTo(LAST_NAME_FIRST_PATIENT);
+        }
+
+        @Test
+        @DisplayName("when server send json with two objects then should return List<ClientResponseDto> with size 2")
+        void whenServerSendTwoObjects_ReturnListClientResponseDtoWithSize2() {
+            String jsonBody = """
+                [
+                    {
+                        "agency": "v01",
+                        "createdDateTime": "2021-10-20 03:10:24",
+                        "dob": "1960-02-02",
+                        "firstName": "Mmohn",
+                        "guid": "17DD0E94-6A57-49C8-E933-CC480D1AC3FB",
+                        "lastName": "Mhoms",
+                        "status": "ACTIVE"
+                    },
+                    {
+                        "agency": "v01",
+                        "createdDateTime": "2021-10-20 03:10:47",
+                        "dob": "1955-04-23",
+                        "firstName": "Jmoh",
+                        "guid": "A661E400-8CD0-6336-D0C2-2E8012903819",
+                        "lastName": "Rhomrds",
+                        "status": "ACTIVE"
+                    }
+                ]
+                """;
+            mockServer.enqueue(
+                new MockResponse().setResponseCode(200)
+                    .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .setBody(jsonBody)
+            );
+
+            List<ClientResponseDto> clients = clientWebClient.getClients();
+
+            assertThat(clients.size()).isEqualTo(2);
+        }
     }
 
 
